7805cd7d45a3118e8e365836488b3c149823d82f
Fck-r-sns <fck.r.sns@gmail.com>
2016-11-06 17:49:44 +0300

    Partial implementation

--- k_medoids/KMedoidsClustering.h
+++ k_medoids/KMedoidsClustering.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <functional>
 #include <set>
+#include <random>
 
 template<typename... Types>
 class KMedoidsClustering
@@ -28,9 +29,16 @@ public:
     std::vector<Cluster> go();
 
 private:
+    int getRandomTupleIndex(const std::set<int> &exceptions);
+
+private:
     const Data *m_data = nullptr;
     int m_k = -1;
     DistanceFunc m_distanceFunc;
+
+    std::random_device m_rd;
+    std::mt19937 m_mt = { m_rd() };
+    std::uniform_int_distribution<int> m_rand;
 };
 
 template<typename... Types>
@@ -54,7 +62,43 @@ KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setNumberOfClusters(
 template<typename... Types>
 std::vector<typename KMedoidsClustering<Types...>::Cluster> KMedoidsClustering<Types...>::go()
 {
+    if (!m_data) {
+        throw std::logic_error("Data is NULL");
+    }
+    if (!m_distanceFunc) {
+        throw std::logic_error("Distance function is undefined");
+    }
+    if (m_k == -1) {
+        throw std::logic_error("Number of clusters is undefined");
+    }
+    if (m_k > m_data->size()) {
+        throw std::logic_error("k must be higher than data size");
+    }
+    m_rand.param(decltype(m_rand)::param_type(0, m_data->size()));
+    std::vector<Cluster> result;
+    result.reserve(m_k);
+    std::set<int> usedIndices;
+    for (int i = 0; i < m_k; ++i) {
+        int idx = getRandomTupleIndex(usedIndices);
+        result.emplace_back({ m_data->at(idx) });
+        usedIndices.insert(idx);
+    }
+}
 
+template<typename... Types>
+int KMedoidsClustering<Types...>::getRandomTupleIndex(const std::set<int> &exceptions)
+{
+    const int dataSize = m_data->size();
+    int value, counter = 0;
+    do {
+        value = m_rand(m_mt);
+        ++counter;
+    } while (exceptions.find(value) != exceptions.cend() && counter < dataSize );
+    if (counter > dataSize) {
+        throw std::logic_error("Failed to get random tuple index");
+    } else {
+        return value;
+    }
 }
 
 #endif // KMEDOIDSCLUSTERING_H
