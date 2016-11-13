ac470633f219dae731c144856fcab96ec7458301
Fck-r-sns <fck.r.sns@gmail.com>
2016-11-13 23:01:41 +0300

    Fixes

--- k_medoids/KMedoidsClustering.h
+++ k_medoids/KMedoidsClustering.h
@@ -14,13 +14,13 @@ class KMedoidsClustering
 public:
     using Tuple = std::tuple<Types...>;
     using Data = std::vector<Tuple>;
-    using DistanceFunc = std::function<Tuple(const Tuple &, const Tuple &)>;
-    using ComparatorFunc = std::function<bool(const Tuple &, const Tuple &)>; // operator <
-    using AccumulatorFunc = std::function<void(Tuple &, const Tuple &)>; // arg0 = arg0 + arg1
+    using DistanceFunc = std::function<double(const Tuple &, const Tuple &)>;
 
     struct Cluster {
         Tuple center;
         std::set<int> indices; // indices of data objects which belongs to this cluster
+
+        Cluster(const Tuple &center) : center(center) {}
     };
 
     static const int PROPERTY_COUNT = sizeof...(Types);
@@ -29,21 +29,17 @@ public:
     KMedoidsClustering();
     KMedoidsClustering &setData(const Data *data);
     KMedoidsClustering &setDistanceFunc(const DistanceFunc &func);
-    KMedoidsClustering &setComparatorFunc(const ComparatorFunc &func);
-    KMedoidsClustering &setAccumulatorFunc(const AccumulatorFunc &func);
     KMedoidsClustering &setNumberOfClusters(int k);
     std::vector<Cluster> go();
 
 private:
     int getRandomTupleIndex(const std::set<int> &exceptions);
-    Tuple calculateDistanceSumWithinCluster(const Tuple &tuple, const Cluster &cluster);
+    double calculateDistanceSumWithinCluster(const Tuple &tuple, const Cluster &cluster);
 
 private:
     const Data *m_data = nullptr;
     int m_k = -1;
     DistanceFunc m_distanceFunc;
-    ComparatorFunc m_comparatorFunc;
-    AccumulatorFunc m_accumulatorFunc;
 
     std::random_device m_rd;
     std::mt19937 m_mt;
@@ -60,30 +56,21 @@ template<typename... Types>
 KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setData(const KMedoidsClustering::Data *data)
 {
     m_data = data;
+    return *this;
 }
 
 template<typename... Types>
 KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setDistanceFunc(const DistanceFunc &func)
 {
     m_distanceFunc = func;
-}
-
-template<typename... Types>
-KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setComparatorFunc(const ComparatorFunc &func)
-{
-    m_comparatorFunc = func;
-}
-
-template<typename... Types>
-KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setAccumulatorFunc(const AccumulatorFunc &func)
-{
-    m_accumulatorFunc = func;
+    return *this;
 }
 
 template<typename... Types>
 KMedoidsClustering<Types...> &KMedoidsClustering<Types...>::setNumberOfClusters(int k)
 {
     m_k = k;
+    return *this;
 }
 
 template<typename... Types>
@@ -95,25 +82,22 @@ std::vector<typename KMedoidsClustering<Types...>::Cluster> KMedoidsClustering<T
     if (!m_distanceFunc) {
         throw std::logic_error("Distance function is undefined");
     }
-    if (!m_comparatorFunc) {
-        throw std::logic_error("Comparator function is undefined");
-    }
-    if (m_k == -1) {
+    if (m_k < 0) {
         throw std::logic_error("Number of clusters is undefined");
     }
     const auto dataSize = m_data->size();
-    if (m_k > dataSize) {
+    if (m_k > (int)dataSize) {
         throw std::logic_error("k can not be higher than data size");
     }
 
     // init random number generator
-    m_rand.param(decltype(m_rand)::param_type(0, m_data->size()));
+    m_rand.param(typename decltype(m_rand)::param_type(0, m_data->size()));
     std::vector<Cluster> clusters;
     clusters.reserve(m_k);
     std::set<int> usedIndices;
     for (int i = 0; i < m_k; ++i) {
         int idx = getRandomTupleIndex(usedIndices);
-        clusters.emplace_back({ m_data->at(idx) });
+        clusters.emplace_back(m_data->at(idx));
         usedIndices.insert(idx);
     }
     bool centersChanged = false;
@@ -124,29 +108,30 @@ std::vector<typename KMedoidsClustering<Types...>::Cluster> KMedoidsClustering<T
         }
 
         // distribute objects between clusters
-        std::vector<Tuple> distances;
+        std::vector<double> distances;
         distances.reserve(m_k);
-        for (int dataObjectIdx = 0; dataObjectIdx < dataSize; ++dataObjectIdx) {
+        for (size_t dataObjectIdx = 0; dataObjectIdx < dataSize; ++dataObjectIdx) {
             const Tuple &t = m_data->at(dataObjectIdx);
+            distances.clear();
             for (const Cluster &c : clusters) {
                 distances.emplace_back(m_distanceFunc(c.center, t));
             }
-            const auto min = std::min_element(distances.cbegin(), distances.cend(), m_comparatorFunc);
+            const auto min = std::min_element(distances.cbegin(), distances.cend());
             if (min == distances.cend()) {
                 throw std::logic_error("Failed to find nearest center");
             }
             const int nearestClusterIndex = std::distance(distances.cbegin(), min);
-            clusters.at(nearestClusterIndex).indices.push_back(dataObjectIdx);
+            clusters.at(nearestClusterIndex).indices.insert(dataObjectIdx);
         }
 
         // find new medoids
         centersChanged = false;
         for (Cluster &c : clusters) {
-            Tuple medoidDistance = calculateDistanceSumWithinCluster(c.center, c);
+            double medoidDistance = calculateDistanceSumWithinCluster(c.center, c);
             for (int medoidCandidateIdx : c.indices) {
                 const Tuple &medoidCandidate = m_data->at(medoidCandidateIdx);
-                const Tuple dst = calculateDistanceSumWithinCluster(medoidCandidate, c);
-                if (m_comparatorFunc(dst, medoidDistance)) {
+                const double dst = calculateDistanceSumWithinCluster(medoidCandidate, c);
+                if (dst < medoidDistance) {
                     c.center = medoidCandidate;
                     medoidDistance = dst;
                     centersChanged = true;
@@ -176,14 +161,12 @@ int KMedoidsClustering<Types...>::getRandomTupleIndex(const std::set<int> &excep
 }
 
 template<typename... Types>
-typename KMedoidsClustering<Types...>::Tuple
-KMedoidsClustering<Types...>::calculateDistanceSumWithinCluster(
-        const Tuple &from, const Cluster &cluster)
+double KMedoidsClustering<Types...>::calculateDistanceSumWithinCluster(const Tuple &from, const Cluster &cluster)
 {
-    Tuple distanceSum;
+    double distanceSum;
     for (int tupleIdx : cluster.indices) {
         const Tuple &t = m_data->at(tupleIdx);
-        m_accumulatorFunc(distanceSum, m_distanceFunc(from, t));
+        distanceSum += m_distanceFunc(from, t);
     }
     return distanceSum;
 }
